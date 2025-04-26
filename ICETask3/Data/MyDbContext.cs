a56aaee34b26bf8850f2e05f3cf6e5e4ae169bff
Yavin Mc Pherson <st10438186@vcconnect.edu.za>
2025-04-26 18:23:51 +0200

    Created MVC Program and SQL Local database

---
+++ ICETask3/Data/MyDbContext.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+using System.Collections.Generic;
+using ICETask3.Models;
+using Microsoft.EntityFrameworkCore;
+using Task = ICETask3.Models.Tasks;
+
+namespace ICETask3.Data;
+
+public partial class MyDbContext : DbContext
+{
+    public MyDbContext()
+    {
+    }
+
+    public MyDbContext(DbContextOptions<MyDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Task> Tasks { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Task>(entity =>
+        {
+            entity.HasKey(e => e.TaskId).HasName("PK__Tasks__7C6949D196F17000");
+
+            entity.Property(e => e.TaskId).HasColumnName("TaskID");
+            entity.Property(e => e.TaskDescription)
+                .HasMaxLength(200)
+                .IsUnicode(false);
+            entity.Property(e => e.TaskName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.TaskStatus)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ ICETask3/Models/Task.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ICETask3.Models;
+
+public partial class Tasks
+{
+    public int TaskId { get; set; }
+
+    public string? TaskName { get; set; }
+
+    public string? TaskDescription { get; set; }
+
+    public string? TaskStatus { get; set; }
+}
